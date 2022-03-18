f3f291364265b335bec9ce8c3a2ad2d4eed6548c
“kishanthanki-20” <viral.gajera218@gmail.com>
2022-03-18 22:31:31 +0530

    upload

---
+++ contact.php
@@ -0,0 +1,74 @@
+<!-- Contact -->
+<h1>Contact Us</h1>
+<div class="">
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Reach to Us</div>
+            <!-- <h1>Contact Us</h1><br> -->
+          </div>
+          <div class="contact_text text-center"><br>
+            <p>DO NOT HESITATE TO GET IN TOUCH WITH US.<br>
+We are available 24/7 to help and advise you on the best we have to offer during your stay.</p>
+          </div><br><br>
+          <div class="">
+            <form action="#" class=" text-center">
+              <div class="">
+                <div style="margin: 0 auto" class="">
+                  <input style="border: 3px solid purple;border-radius: 25px;width:350px; height:45px; " type="text" class="" placeholder="  Your name" required>
+                </div><br>
+                <div style="margin: 0 auto" class="">
+                  <input style="border: 3px solid purple;border-radius: 25px;width:350px; height:45px; " type="email" class="textbox" placeholder=" Your email" required>
+                </div><br>
+              </div>
+              <div class="">
+                <div style="margin: 0 auto" class="">
+              <input style="border: 3px solid purple;border-radius: 25px;width:350px; height:45px; " type="text" class="" placeholder="  Subject">
+                </div><br>
+                <div style="margin: 0 auto" class="">
+              <textarea style="border: 3px solid purple;border-radius: 25px;width:350px; height:45px; " class="" placeholder="  Message" required="required"></textarea>
+              </div><br>
+              </div>
+              <button style="border-radius: 25px;  
+    background-color: orange;  
+    color:#FFFFFF;  
+    text-align :center;  
+    font-family:arial, helvetica, sans-serif;  
+    padding: 5px 10px 10px 10px;  
+    font-weight:bold;  
+    width:150px;  
+    height:70px; " class="">&nbsp;&nbsp;Send Message&nbsp;&nbsp;</button><br>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Map -->
+  <!-- <div class="contact_map_container">
+    <div class="map">
+      <div id="google_map" class="google_map">
+        <div class="map_container">
+          <div id="map"></div>
+        </div>
+      </div>
+    </div> -->
+
+  <!-- Contact Map Content -->
+  <div class="contact_map_content">
+    <div class="d-flex flex-column align-items-center justify-content-center">
+      <br><img class="contact_info_logo_2" src="images/hotel_asha_logo.png" alt=""><br>
+      <div class="contact_info_list">
+        <ul class="text-center">
+          <li>Jamnagar Khambhadliya Highwat, Sikka Patia, Gujarat 361 001</li>
+          <li>weareasha@hotelasha.com</li>
+          <li>91-9988776655</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
